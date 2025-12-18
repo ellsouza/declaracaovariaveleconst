921f8df55b40716e6fdc38f8ed0f7788480aabed
Ellen Souza de Almeida <120521783+ellsouza@users.noreply.github.com>
2025-12-17 21:22:43 -0300

    Add user greeting and server entry log

---
+++ style.js
@@ -0,0 +1,7 @@
+const nickname = prompt ("Digite o seu nome de usuário")
+console.log("Digite o seu nome de usuário")
+
+let exclamacao = "!"
+
+console.log("Seja bem-vindo(a), " + nickname + exclamacao)
+console.log(nickname + " entrou no servidor")
