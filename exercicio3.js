140f6a515d17322b9be89b330d87497a5614b17e
Ellen Souza de Almeida <120521783+ellsouza@users.noreply.github.com>
2025-12-20 17:56:58 -0300

    Change coffee variable and update console log
    Updated the coffee variable and logged the new value.

---
+++ exercicio3.js
@@ -0,0 +1,10 @@
+let poteCafe = "Café Pilão"
+let poteAcucar = "Açucar cristalizado"
+let poteBisc = "Biscoitos amanteigados"
+
+console.log("Na cozinha da vovó tem: " + poteCafe + " - " + poteAcucar + " - " + poteBisc + ".")
+
+//alteração de variável;
+poteCafe = "Café Pelé"
+
+console.log("A vovó tem " + poteCafe + " na dispensa da cozinha.")
